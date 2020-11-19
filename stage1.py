8a7451ea05c310e33e21dec4bed372d38e2d9d69
marydcodes <40235658+marydCodes@users.noreply.github.com>
2020-11-19 11:57:59 +0000

    completed stage1

--- stage1.py
+++ stage1.py
@@ -1,2 +1,13 @@
 # Stage 1: Unfair computer. Programme should always choose an option that defeats the one picked by the user
 
+def janken1():
+    user_input = str(input())
+    if user_input == "rock":
+        print("Sorry, but the computer chose paper")
+    elif user_input == "paper":
+        print("Sorry, but the computer chose scissors")
+    elif user_input == "scissors":
+        print("Sorry, but the computer chose rock")
+
+
+janken1()
